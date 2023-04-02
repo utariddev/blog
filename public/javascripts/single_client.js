f0521d0b068acae33a7124c8f7534aeac014925c
utarid <utarid@protonmail.com>
2023-04-02 11:21:34 +0200

    added highlight.js

--- public/javascripts/single_client.js
+++ public/javascripts/single_client.js
@@ -213,4 +213,5 @@ function locateArticle(serverData) {
                 </script>
                 <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
     `)
+    hljs.highlightAll();
 }

49290b58c660e40dbdb7c860b2848b214086a62a
utarid <utarid@protonmail.com>
2023-05-12 23:14:03 +0200

    updated sitemap

--- routes/sitemap.js
+++ routes/sitemap.js
@@ -6,6 +6,10 @@ router.get('/', function(req, res, next) {
     res.send(`<?xml version="1.0" encoding="UTF-8"?>
         <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
             <url>
+                <loc>https://utarid.org/single/spring-boot-ta-validation-yapmak</loc>
+                <lastmod>2023-05-08T00:00:00+00:00</lastmod>
+            </url>
+            <url>
                 <loc>https://utarid.org/single/linux-te-wifi-yapilandirmasi</loc>
                 <lastmod>2023-05-08T00:00:00+00:00</lastmod>
             </url>

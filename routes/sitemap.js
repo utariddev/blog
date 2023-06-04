845fc50c1945d8aebfd19df02d524dc6a5e54b71
utarid <utarid@protonmail.com>
2023-06-04 23:00:28 +0200

    updated sitemap

--- routes/sitemap.js
+++ routes/sitemap.js
@@ -6,6 +6,14 @@ router.get('/', function(req, res, next) {
     res.send(`<?xml version="1.0" encoding="UTF-8"?>
         <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
             <url>
+                <loc>https://utarid.org/single/concurrency</loc>
+                <lastmod>2023-06-04T00:00:00+00:00</lastmod>
+            </url>
+            <url>
+                <loc>https://utarid.org/single/busy-spin</loc>
+                <lastmod>2023-06-04T00:00:00+00:00</lastmod>
+            </url>
+            <url>
                 <loc>https://utarid.org/single/spring-boot-restTemplate</loc>
                 <lastmod>2023-05-27T00:00:00+00:00</lastmod>
             </url>

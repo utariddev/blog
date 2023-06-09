80b3493e195623d967a52c7942f8ffe1eaa58217
utarid <utarid@protonmail.com>
2023-06-09 22:09:35 +0200

    updated sitemap

--- routes/sitemap.js
+++ routes/sitemap.js
@@ -7,7 +7,23 @@ router.get('/', function(req, res, next) {
         <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
             <url>
                 <loc>https://utarid.org/sitemap</loc>
-                <lastmod>2023-06-08T00:00:00+00:00</lastmod>
+                <lastmod>2023-06-09T00:00:00+00:00</lastmod>
+            </url>
+            <url>
+                <loc>https://utarid.org/single/oop-kavramlari:-aggregation</loc>
+                <lastmod>2023-06-09T00:00:00+00:00</lastmod>
+            </url>
+            <url>
+                <loc>https://utarid.org/single/oop-kavramlari</loc>
+                <lastmod>2023-06-09T00:00:00+00:00</lastmod>
+            </url>
+            <url>
+                <loc>https://utarid.org/single/oop-kavramlari:-composition</loc>
+                <lastmod>2023-06-09T00:00:00+00:00</lastmod>
+            </url>
+            <url>
+                <loc>https://utarid.org/single/liskov-substitution-principle</loc>
+                <lastmod>2023-06-09T00:00:00+00:00</lastmod>
             </url>
             <url>
                 <loc>https://utarid.org/single/auto-boxing</loc>

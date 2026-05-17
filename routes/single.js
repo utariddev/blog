ca2ffa4dd2778f1725773e3ff095d148bbe586c8
utariddev <utariddev@gmail.com>
2026-05-17 07:31:27 +0200

    initial commit for angular

--- routes/single.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-// router.get('/', function(req, res, next) {
-//   debug("single");
-//   res.render('single', {});
-// });
-
-module.exports = router;
